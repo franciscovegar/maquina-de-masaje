0e4f34b11fbb6e82c774a37bdec767377a48c10e
franciscovegar <99676222+franciscovegar@users.noreply.github.com>
2022-03-07 09:56:52 -0300

    Add files via upload

---
+++ gsr.ino
@@ -0,0 +1,22 @@
+#include <SPI.h>
+
+const int GSR=34;
+int sensorValue=0;
+int gsr_average=0;
+ 
+void setup(){
+  Serial.begin(9600);
+}
+ 
+void loop(){
+  long sum=0;
+  for(int i=0;i<10;i++)           //Average the 10 measurements to remove the glitch
+      {
+      sensorValue=analogRead(GSR);
+      sum += sensorValue;
+      delay(5);
+      }
+   gsr_average = sum/10;
+   Serial.println(gsr_average);
+   delay(500);
+}
